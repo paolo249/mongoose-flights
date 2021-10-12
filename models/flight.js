3c4a2fdf512f5a13a4442accb3e20beb925dc68d
paolo249 <90116237+paolo249@users.noreply.github.com>
2021-10-12 00:45:12 -0700

    revision on show file

--- models/flight.js
+++ models/flight.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema; 
 
 const destinationSchema = new Schema({
+    content: String,
     airport: {
         type: String, 
         enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
-    }, 
+    },
     arrival: {
         type: Date
     }
-});
+    }, {
+        timestamps: true
+    });
 
 
 const flightSchema = new Schema({
@@ -44,6 +47,8 @@ const flightSchema = new Schema({
         enum: ['LANDED','DELAYED','BOARDED']
     },
     destinations: [destinationSchema]
-});
+    }, {
+        timestamps: true
+    });
 
 module.exports = mongoose.model('Flight', flightSchema);
